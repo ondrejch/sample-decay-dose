d99513498f1cb0ff6743a59b87252006dde999df
Ondrej Chvala <ondrejch@gmail.com>
2025-02-13 15:09:20 -0500

    stellite analysis

---
+++ examples/co_stellite/stell_irr_doses.py
@@ -0,0 +1,56 @@
+#!/bin/env python3
+"""
+Example use case of SampleDose - irradiation of stellite
+Ondrej Chvala <ochvala@utexas.edu>
+"""
+
+from sample_decay_dose import SampleDose
+from sample_decay_dose.SampleDose import extract_flux_values
+import numpy as np
+import json5
+import os
+import re
+
+cwd: str = os.getcwd()
+stellite_mass: float = float(re.findall(r'_([\d.]+)g', cwd)[0])
+irradiation_years: float = float(re.findall(r'dose-([\d.]+)year_', cwd)[0])
+
+stellite_adens: dict = {'c-12': 0.001136285, 'c-13': 1.228975e-05, 'cr-50': 0.001152783, 'cr-52': 0.02223027,
+    'cr-53': 0.002520734, 'cr-54': 0.000627464, 'co-59': 0.05488167, 'ni-58': 0.001454611, 'ni-60': 0.0005603136,
+    'ni-61': 2.435644e-05, 'ni-62': 7.765899e-05, 'ni-64': 1.977746e-05, 'mo-92': 0.000405459, 'mo-94': 0.0002533775,
+    'mo-95': 0.0004364792, 'mo-96': 0.0004578914, 'mo-97': 0.0002624366, 'mo-98': 0.0006640524, 'mo-100': 0.0002654562}
+
+scale_out: str = os.path.expanduser('~/0.02/10-upper_enclosure_doses/20-stellite/01-triton/msrr.out')
+
+flux_data = extract_flux_values(scale_out)
+stellite_flux: float = flux_data[9000]
+print(f'Stellite flux = {stellite_flux} n/cm2/s, mass: {stellite_mass} g, irradiate for {irradiation_years} years')
+
+r = {}
+d = {}
+for decay_days in np.geomspace(1. / 24., 360, 60):
+    irr = SampleDose.OrigenIrradiation('../stellite.f33', stellite_mass)
+    irr.set_decay_days(decay_days)
+    irr.irradiate_days = irradiation_years * 365.24
+    irr.irradiate_flux = stellite_flux  # n/s/cm2
+    irr.write_atom_dens(stellite_adens)
+    irr.run_irradiate_decay_sample()
+
+    mavric = SampleDose.DoseEstimator(irr)
+    mavric.run_mavric()
+    mavric.get_responses()
+
+    print(mavric.responses)
+    # print(mavric.total_dose)
+
+    r[decay_days] = mavric.responses
+    d[decay_days] = mavric.total_dose
+
+print(r)
+print(d)
+
+with open('responses.json', 'w') as fout:
+    json5.dump(r, fout, indent=4)
+
+with open('doses.json', 'w') as fout:
+    json5.dump(d, fout, indent=4)
---
+++ examples/co_stellite/stell_irr_plot_gamma.py
@@ -0,0 +1,66 @@
+#!/bin/env python3
+"""
+Plotting script for calc_doses_mass
+Ondrej Chvala <ochvala@utexas.edu>
+"""
+
+import os
+import re
+import numpy as np
+import json5
+import matplotlib.pyplot as plt
+from sample_decay_dose.SampleDose import extract_flux_values
+
+scale_out: str = os.path.expanduser('~/0.02/10-upper_enclosure_doses/20-stellite/01-triton/msrr.out')
+flux_data = extract_flux_values(scale_out)
+stellite_flux: float = flux_data[9000]
+
+cwd: str = os.getcwd()
+stellite_mass: float = float(re.findall(r'_([\d.]+)g', cwd)[0])
+irradiation_years: float = float(re.findall(r'dose-([\d.]+)year_', cwd)[0])
+print(f'stellite flux: {stellite_flux} n/cm2/s, mass: {stellite_mass} g, irradiated for {irradiation_years} years')
+
+# For sample mass dependency, set LABEL='m'.
+LABEL = 'irr1'
+labels = {'irr1': ['decay time', 'days', f'Stellite {irradiation_years} years irradiation at {stellite_flux:.1e} n/cm2/s, {stellite_mass:.1f} g'], }
+
+# particles = {'1': 'Neutron', '2': 'Gamma', '3': 'Beta'}
+particles = {'2': 'Gamma'}
+data = {'.': 'sandybrown',}
+#    'co_0.05pct': 'slategrey', 'co_0.10pct': 'cornflowerblue', 'co_0.20pct': 'crimson', 'co_0.40pct': 'orange'}
+
+dose = {}  # doses [rem/h]
+errd = {}  # stdev of doses
+r = {}
+
+p = '2'  # Gamma only
+for d in data.keys():
+    with open(os.path.join(d, 'responses.json')) as fin:
+        r[d] = json5.load(fin)
+        dose[d] = np.array([v[p]['value'] for k, v in r[d].items()], float)
+        errd[d] = np.array([v[p]['stdev'] for k, v in r[d].items()], float)
+
+x = np.array(list(r[list(data.keys())[0]].keys()), float)  # x coordinate - sample masses
+
+# Plots!
+plt.close('all')
+plt.xscale('linear')
+plt.yscale('linear')
+plt.grid()
+plt.title(labels[LABEL][2])
+plt.xlabel(f'Sample {labels[LABEL][0]} [{labels[LABEL][1]}]')
+plt.ylabel('Dose at 30 cm [rem/h]')
+
+for d in data.keys():
+    ytitle = f'Stellite cylinder, {stellite_mass:.1f} g'
+    plt.errorbar(x, dose[d], errd[d], ls='none', color=f'{data[d]}', capsize=0.8)
+    plt.scatter(x, dose[d], color=f'{data[d]}', s=5, label=ytitle)
+
+plt.legend()
+plt.tight_layout()
+label_file_name = labels[LABEL][0].replace(' ', '_')
+plt.savefig(f'dose_stellite_{label_file_name}.png', dpi=1000)
+
+plt.xscale('log')
+plt.yscale('log')
+plt.savefig(f'dose_stellite_{label_file_name}-loglog.png', dpi=1000)
